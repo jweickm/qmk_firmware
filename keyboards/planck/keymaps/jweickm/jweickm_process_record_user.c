b26f189917af25f8f105d9b342a2d71e93655158
jweickm <jakob.weickmann@posteo.de>
2022-02-06 14:16:15 +0100

    enter now properly leaves caps lock

--- keyboards/planck/keymaps/jweickm/jweickm_process_record_user.c
+++ keyboards/planck/keymaps/jweickm/jweickm_process_record_user.c
@@ -326,9 +326,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 layer_off(_NUM);
             }
             return true;
+        case RSFT_T(KC_ENT):
         case RAISE:
+            if (record->tap.count && record->event.pressed) {
+                tap_code(KC_CAPS);
+            } 
+            return true;
         case KC_ENT:
-        case KC_SFTENT:
             if (record->event.pressed && caps_lock_on) {
                 tap_code(KC_CAPS);
             }
