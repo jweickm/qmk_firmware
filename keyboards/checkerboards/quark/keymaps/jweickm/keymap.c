13f0cc9c57e1335a1d98b09ce4c1a93248b8e7ca
jweickm <jakob.weickmann@posteo.de>
2022-09-11 12:43:48 +0200

    Middle key on naginata is now enter.

--- keyboards/checkerboards/quark/keymaps/jweickm/keymap.c
+++ keyboards/checkerboards/quark/keymaps/jweickm/keymap.c
@@ -344,14 +344,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |------+------+------+------+------+------+------+------+------+------+------+------|
      * | ____ |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | ____ |
      * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | ____ | ____ | ____ | ____ |  SPC |    _____    |  SPC | ____ | ____ | ____ | ____ | 1x2uC 
+     * | ____ | ____ | ____ | ____ |  SPC |      ENT    |  SPC | ____ | ____ | ____ | ____ | 1x2uC 
      * `-----------------------------------------------------------------------------------'
      */
     [_QWERTY] = LAYOUT_planck_mit(
         KC_TRNS, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, TD(TD_BRC), 
         KC_TRNS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
         KC_TRNS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_TRNS, 
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, NAVSPACE, KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS/*FN_KEY*/
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC, ENT_KEY, KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS/*FN_KEY*/
     ),
 #endif 
 
